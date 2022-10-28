6e13a7df17c90741b6670020644b566a24bfd514
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-10-28 18:07:50 +0200

    Add and render character modal component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { ethers } from "ethers";
 import WalletCard from './components/WalletCard';
 import Navbar from './components/Navbar'
+import CharacterModal from "./components/CharacterModal";
 import './App.css';
 
 export default function App() {
@@ -12,29 +13,33 @@ export default function App() {
   }
   
   return (
-    <section className="main-container">
-      <Navbar isConnected={isConnected}/>
+    <main className="main-container">
+      <section className="banner-container">
+        <Navbar isConnected={isConnected}/>
 
-      <div className="content-wrapper">
-        <div className="dataContainer">
-          <div className="header">
-            Hello Blockchain Enthusiast! <span have role="img" aria-label="Emoji">👋</span>
+        <div className="content-wrapper">
+          <div className="dataContainer">
+            <div className="header">
+              Hello Blockchain Enthusiast! <span have role="img" aria-label="Emoji">👋</span>
+            </div>
+
+            <p className="greeting">Welcome to my Smart Portal</p>
+
+            <div className="bio">
+              <p>I am Simo, a full stack developer aspiring to be a Smartchain developer.</p>
+              <p>Connect your Ethereum wallet and tag me with your favorite pokemon!</p>
+            </div>
+
+            <WalletCard
+              wave={wave}
+              isConnected={isConnected}
+              setIsConnected={setIsConnected}
+            />
           </div>
-
-          <p className="greeting">Welcome to my Smart Portal</p>
-
-          <div className="bio">
-            <p>I am Simo, a full stack developer aspiring to be a Smartchain developer.</p>
-            <p>Connect your Ethereum wallet and tag me with your favorite pokemon!</p>
-          </div>
-
-          <WalletCard
-            wave={wave}
-            isConnected={isConnected}
-            setIsConnected={setIsConnected}
-          />
         </div>
-      </div>
-    </section>
+      </section>
+
+      <CharacterModal />
+    </main>
   );
 }
---
+++ src/components/CharacterModal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CharacterCard from './CharacterCard'
+
+function CharacterModal() {
+  return (
+    <section className="modal-container">
+        <CharacterCard />
+    </section>
+  )
+}
+
+export default CharacterModal
