97820c53a2978c985567176a9f1b77cdc96fee7e
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-11-01 21:16:24 +0200

    Fetch data from Blockchain

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import Navbar from './components/Navbar'
 import CharacterModal from "./components/CharacterModal";
 import CharacterData from './components/api';
 import './App.css';
+import abi from "./utils/SmartPortal.json";
 
 export default function App() {
   const [isConnected, setIsConnected] = React.useState(false);
   const [modalIsOpen, setModalIsOpen] = React.useState(true);
   const [characters, setCharacters] = React.useState(CharacterData);
+  const contractAddress = "0x9E66864a9A43c21a970F15fc63A32d25B735BF94"; //contract deployment address
+  const contractABI = abi.abi; //reference the abi content
 
-  const wave = () => {
-    setModalIsOpen(!modalIsOpen)
-  }
+  const viewCharacters = () => setModalIsOpen(!modalIsOpen);
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+      // setModalIsOpen(!modalIsOpen)
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getWaveStatus();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+}
   
   return (
     <main className="main-container">
@@ -30,11 +51,11 @@ export default function App() {
 
             <div className="bio">
               <p>I am Simo, a full stack developer aspiring to be a Smartchain developer.</p>
-              <p>Connect your Ethereum wallet and tag me with your favorite pokemon!</p>
+              <p>Connect your Ethereum wallet and tag me with your favorite superhero or villain!</p>
             </div>
 
             <WalletCard
-              wave={wave}
+              viewCharacters={viewCharacters}
               isConnected={isConnected}
               setIsConnected={setIsConnected}
             />
@@ -43,6 +64,7 @@ export default function App() {
       </section>
 
       <CharacterModal
+        wave={wave}
         modalIsOpen={modalIsOpen}
         setModalIsOpen={setModalIsOpen}
         characters={characters}
